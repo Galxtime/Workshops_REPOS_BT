e66acca5db9fb93cbc66595c185a21292c7f86c0
Galxtime <gllrmacosta@yahoo.com>
2020-10-06 10:45:45 -0500

    add files POKEAPI

--- PokeAPI_Workshop/assets/js/getData.js
+++ PokeAPI_Workshop/assets/js/getData.js
@@ -1,15 +1,11 @@
-//Url APi save as variable
-
-
-
-
 
 //Get values from API (Get)
 
 let lista = document.getElementById("listpokemon")
 let game= document.getElementById("game");
 
-const getData =(id, num) => {
+
+const getDataPoki =(id, num) => {
 
     return  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 
@@ -34,8 +30,8 @@ const getData =(id, num) => {
 const randomPokiGenerator = () => {
     let firstId  = Math.floor(Math.random() *50);
     let secondId = Math.floor(Math.random() *50);
-    getData(firstId, 1);
-    getData(secondId, 2);
+    getDataPoki(firstId, 1);
+    getDataPoki(secondId, 2);
 }
 
 
@@ -56,6 +52,71 @@ const pokiCheckData = (pokemon, num) => {
 //run function get Data from API
 game.onclick = function (){
     randomPokiGenerator()
-}
+} 
+
+
+
+   //URL de la API
+const API = "https://pokeapi.co/api/v2/pokemon?limit=30&offset=00";
+
+// Obtener resultado de API
+const getData = (api) => {
+    return fetch(api)
+        .then((response) => response.json())
+        .then((json) => {
+            llenarDatos(json), paginacion(json);
+        })
+        .catch((error) => {
+            console.log("Error :", error);
+        });
+};
+
+const llenarDatos = (data) => {
+    let html = "";
+    document.getElementById("datosPersonajes").innerHTML= "";
+    data.results.forEach((pj) => {
+        const pokeURL = pj.url;
+        return fetch(pokeURL)
+        
+            .then((response) => response.json())
+            .then((json) => {
+                fillOutPics(json,html)
+            })
+            .catch((error) => {
+                console.log("Error :", error);
+            });
+    });
+};
+
+const fillOutPics = (data, html) => {
+
+        
+        html += '<div class="col mt-5">';
+        html += '<div class="card" style="width: 10rem;">';
+        html += `<img src="${data.sprites.other.dream_world.front_default}" class="card-img-top" alt="...">`;
+        html += '<div class="card-body">';
+        html += `<h5 class = "card-title" >${data.name}</h5>`;
+        html += `<p class="card-text">Altura :${data.height}</p>`;
+        html += `<p class="card-text">Peso :${data.weight}</p>`;
+        html += "</div>";
+        html += "</div>";
+        html += "</div>";
+       document.getElementById("datosPersonajes").innerHTML += html;
+};
+
+// Paginacion
+const paginacion = (info) => {
+   
+    let html = "";
+    html += `<li class="page-item ${info.previous ? "" :"disabled"}"><a class="page-link" onclick="getData('${info.previous}')">Previous</a></li>`;
+    html += `<li class="page-item ${info.next ? "" : "disabled"}"><a class="page-link" onclick="getData('${info.next}')">Next</a></li>`;
+    document.getElementById("paginacion").innerHTML = html;
+};
+
+//EjecutargetData
+getData(API); 
+
+
+
 
 
